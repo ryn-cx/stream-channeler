632abc86f4a4dfafb9f887200903097743b69637
ryn-cx <262888154+ryn-cx@users.noreply.github.com>
2026-07-03 05:07:36 -0700

    Improve downloading logging

--- backend/app/log.py
+++ backend/app/log.py
@@ -1,4 +1,5 @@
 # TODO: Validate
+import logging
 import sys
 
 from loguru import logger
@@ -11,7 +12,26 @@ _STDOUT_FORMAT = (
 )
 
 
+class InterceptHandler(logging.Handler):  # noqa: D101
+    def emit(self, record: logging.LogRecord) -> None:  # noqa: D102
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = logging.currentframe(), 2
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.bind(source=record.name).opt(
+            depth=depth,
+            exception=record.exc_info,
+        ).log(level, record.getMessage())
+
+
 def configure_logging() -> None:  # noqa: D103
     logger.remove()
     logger.configure(extra={"source": "app"})
     logger.add(sys.stdout, level="INFO", colorize=True, format=_STDOUT_FORMAT)
+    logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
